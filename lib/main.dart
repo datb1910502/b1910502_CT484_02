b1ba3dd89a52307505b3c8659489b32ddd3917d2
dat <datb1910502@student.ctu.edu.vn>
2022-10-17 13:47:02 +0700

    xay dung trang cac dat hang

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'ui/products/product_manager.dart';
 import 'ui/products/product_overview_screen.dart';
 import 'ui/products/user_products_screen.dart';
 import 'ui/cart/cart_screen.dart';
+import 'ui/order/order_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       home: const SafeArea(
-        child: CartScreen(),
+        child: OrdersScreen(),
       )
 
     );
